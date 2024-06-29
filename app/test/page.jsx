f3b8d530022d99ddd2f32af2c7f6b51d8819133d
Nikunj <nikunj@adhocorder.ro>
2024-06-30 04:47:30 +0530

    test added

---
+++ app/test/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='flex justify-center items-center h-screen '>
+            <button onClick={() => window.location.href = "https://amzn.to/45KRRG8"} className='border text-white px-3 py-1 rounded-lg'>Click me</button>
+        </div>
+    )
+}
+
+export default page
